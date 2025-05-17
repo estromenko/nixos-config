1e1a306eb48eec969552c737cfcce94c49ff7960
estromenko <estromenko@mail.ru>
2025-05-17 14:30:44 +0300

    Fix ironbar color theme

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,19 @@
     ];
     nixpkgs.config.allowUnfree = true;
 
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+    };
+
     home.packages = with pkgs; [
       networkmanagerapplet
       pavucontrol
