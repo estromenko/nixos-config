2f49182e8d41dfb81ab1087eb0965db582a43f4c
estromenko <estromenko@mail.ru>
2025-05-31 11:10:10 +0300

    Add language servers to helix configuration and change theme to tokyonight

--- hosts/estromenko/home-manager/home.nix
+++ hosts/estromenko/home-manager/home.nix
@@ -66,7 +66,49 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    settings.theme = "gruvbox";
+    extraPackages = with pkgs; [
+      cargo
+    ];
+    settings = {
+      keys.normal.space.e = "file_picker_in_current_buffer_directory";
+      theme = "tokyonight";
+    };
+    languages = {
+      language = [
+        {
+          name = "rust";
+          language-servers = ["rust-analyzer"];
+          file-types = ["rs"];
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.rustfmt}/bin/rustfmt";
+          };
+        }
+        {
+          name = "python";
+          language-servers = ["ty" "pyright"];
+          file-types = ["py"];
+          roots = ["pyproject.toml" "setup.py" "Poetry.lock" ".git" ".venv"];
+          comment-token = "#";
+          shebangs = ["python" "python3"];
+        }
+      ];
+      language-server = {
+        rust-analyzer = {
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+          checkOnSave.command = "${pkgs.clippy}/bin/clippy";
+          procMacro.enable = true;
+        };
+        ty = {
+          command = "${pkgs.ty}/bin/ty";
+          args = ["server"];
+          experimental.completions.enable = true;
+        };
+        pyright = {
+          command = "${pkgs.pyright}/bin/pyright-langserver";
+        };
+      };
+    };
   };
 
   programs.yazi = {
