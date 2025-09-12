cb4d3cce171f2c705de414c156e7c04b191e4054
estromenko <estromenko@mail.ru>
2025-09-12 23:30:14 +0300

    Update flake.lock, but disable sxc

--- hosts/estromenko/hardware-configuration.nix
+++ hosts/estromenko/hardware-configuration.nix
@@ -37,7 +37,6 @@
   swapDevices = [];
 
   zramSwap.enable = true;
-  services.scx.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
